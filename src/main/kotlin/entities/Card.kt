2e69b8eebfc4263304e9c949edb5e16d84a175b4
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-10-08 17:34:30 -0300

    :sparkles: feat: implements data abstraction for the Card class

--- src/main/kotlin/entities/Card.kt
+++ src/main/kotlin/entities/Card.kt
@@ -1,4 +1,46 @@
 package entities
 
-class Card {
+class Card(
+    private val name: String,
+    private val description: String,
+    private var attack: Int,
+    private var defense: Int,
+    private val type: String,
+    private var mode: String,
+    private var allowStateChange: Boolean
+) {
+    private val equipment: MutableList<Card> = mutableListOf()
+
+    var attackValue: Int
+        get() = attack
+        set(value) {
+            if (value >= 0) {
+                attack = value
+            } else {
+                throw IllegalArgumentException("O valor de 'attack' deve ser não negativo.")
+            }
+        }
+
+    var defenseValue: Int
+        get() = defense
+        set(value) {
+            if (value >= 0) {
+                defense = value
+            } else {
+                throw IllegalArgumentException("O valor de 'defense' deve ser não negativo.")
+            }
+        }
+
+    var cardMode: String
+        get() = mode
+        set(value) {
+            mode = value
+        }
+
+    var stateChangeAllowed: Boolean
+        get() = allowStateChange
+        set(value) {
+            allowStateChange = value
+        }
+
 }
